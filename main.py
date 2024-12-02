bdc55b7d92d947a748c9be09101ed24831a92d7c
Ksharma127 <kris.sharma.2009@gmail.com>
2024-12-02 20:02:36 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#making a Dictionary
+#dictionary is a collection of items in a key value pair similar to list but without index numbers
+
+person={"name":"Krishiv","age":"15","hobbies":"going to the gym"}
+print(person)
+
+#access the values using keys
+print(person["name"])
+
+print(person["hobbies"])
+
+#adding new item to the dictionary
+
+person["country"]= "england" #counrty is a key and england is a value for it
+
+print(person)
